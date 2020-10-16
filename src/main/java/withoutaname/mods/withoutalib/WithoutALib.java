3a6697f68760afb4d3d5c0fc39f038147c9208b6
WithoutAName25 <withoutaname25@gmail.com>
2020-10-16 15:06:46 +0200

    Initial commit

---
+++ src/main/java/withoutaname/mods/withoutalib/WithoutALib.java
@@ -0,0 +1,32 @@
+package withoutaname.mods.withoutalib;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("withoutalib")
+public class WithoutALib {
+
+	public WithoutALib() {
+		// Register the setup method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void setup(final FMLCommonSetupEvent event) {
+
+	}
+
+	// You can use EventBusSubscriber to automatically subscribe events on the
+	// contained class (this is subscribing to the MOD
+	// Event bus for receiving Registry Events)
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents {
+
+	}
+
+}
